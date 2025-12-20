57804f9ee97e18d4ac51ffdcb2305327c8c35f10
Konstantin Kirik (snegovick) <snegovick@uprojects.org>
2025-12-20 03:45:50 +0300

    Add command and pipeline definitions

--- module/sh9util.m
+++ module/sh9util.m
@@ -3,6 +3,21 @@ Sh9Util: module
 PATH: con "/dis/lib/sh9util.dis";
 DESCR: con "Utility functions for sh9";
 
+Command: adt
+{
+	args: list of string;
+	inf, outf: string;
+	append: int;
+};
+
+Async, Seq: con iota;
+
+Pipeline: adt
+{
+	cmds: list of ref Command;
+	term: int;
+};
+
 reverse_list: fn[T](toks: list of T): list of T;
 to_array: fn[T](toks: list of T): array of T;
 };
